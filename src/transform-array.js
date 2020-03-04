af53f667bd8bf228bfd23668e25b6fe765359c2f
Volkau Aliaksey <bobrui4anin@gmail.com>
2020-03-04 18:24:40 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,50 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!(Array.isArray(arr))) {
+        throw 'Its not array';
+    }
+    if (arr.length == 0) {
+        return arr;
+    }
+
+    let tempArr = [];
+
+    for (let i = 0; i < arr.length;) {
+        if (arr[i] == '--discard-next' && i != arr.length - 1) {
+            i += 2;
+            continue;
+        } else if (arr[i] == '--discard-next' && i == arr.length - 1) {
+            break;
+        }
+
+        if (arr[i] == '--discard-prev' && i != 0) {
+            tempArr.splice(tempArr.length - 1, 1);
+            i++;
+            continue;
+        } else if (arr[i] == '--discard-prev' && i == 0) {
+            i++;
+            continue;
+        }
+
+        if (arr[i] == '--double-next' && i != arr.length - 1) {
+            tempArr.push(arr[i + 1]);
+            i++;
+            continue;
+        } else if (arr[i] == '--double-next' && i == arr.length - 1) {
+            break;
+        }
+
+        if (arr[i] == '--double-prev' && i != 0) {
+            tempArr.push(arr[i - 1]);
+            i++;
+            continue;
+        } else if (arr[i] == '--double-prev' && i == 0) {
+            i++;
+            continue;
+        }
+
+        tempArr.push(arr[i]);
+        i++;
+    }
+
+    return tempArr;
 };
