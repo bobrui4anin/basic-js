94746c6c0d33f06d76d2c23324b43c8990188bd6
Volkau Aliaksey <bobrui4anin@gmail.com>
2020-03-04 15:09:02 +0300

    feat: add whats-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if ((new Date(date)) == 'Invalid Date' || isNaN(Date.parse(date)) || date.hasOwnProperty('toString')) {
+    throw 'Incorrect data';
+  }
+
+  let numMonth = date.getMonth() + 1,
+    fallOrAutumn = date.getFullYear() < 1755 ? 'autumn' : 'fall';
+  return numMonth >= 3 && numMonth <= 5 ? 'spring' : numMonth >= 6 && numMonth <= 8 ? 'summer' : numMonth >= 9 && numMonth <= 11 ? fallOrAutumn : numMonth == 12 || numMonth <= 2 ? 'winter' : false;
 };
